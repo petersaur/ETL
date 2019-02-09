4e5c9f4fa53bfc9c8e84be5ed9bc4b4ab584d705
hijohnnyz <johnhuzhao@gmail.com>
2019-02-09 09:45:30 -0800

    updated files

--- ETL.sql
+++ ETL.sql
@@ -1,6 +1,11 @@
 CREATE DATABASE finance_db;
 USE finance_db;
 
-
 SELECT * FROM btc_info;
 SELECT * FROM stock_info;
+
+ALTER TABLE btc_info ADD PRIMARY KEY(Date(12));
+
+SELECT * FROM btc_info
+JOIN date ON btc_info.Date = stock_info.Date
+GROUP BY  Date;
