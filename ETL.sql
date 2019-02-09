a2eec7b0ce9f230005dce937e623fbaa3b7457a4
hijohnnyz <johnhuzhao@gmail.com>
2019-02-09 11:25:02 -0800

    final add

--- ETL.sql
+++ ETL.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE finance_db;
 USE finance_db;
 
-SELECT * FROM btc_info;
-SELECT * FROM stock_info;
+ALTER TABLE btc_info ADD PRIMARY KEY (Date(255));
 
-ALTER TABLE btc_info ADD PRIMARY KEY(Date(12));
+CREATE TABLE combined_table (
+SELECT * 
+FROM btc_info INNER JOIN stock_info
+USING (Date))
+;
 
-SELECT * FROM btc_info
-JOIN date ON btc_info.Date = stock_info.Date
-GROUP BY  Date;
+SELECT * FROM combined_table;
